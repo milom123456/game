b6a883f785f0b4f8e6713483a1123ac2dd127e91
milon <eer51889@gmail.com>
2026-02-08 17:57:56 +0500

    Fix Railway frontend deploy

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,3 +1,5 @@
+import axios from "axios";  // 🔑 import না থাকলে error হবে
+
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" }
